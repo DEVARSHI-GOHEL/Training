ef3fda41465601603d2036e50a29ca142e523282
DEVARSHI-GOHEL <97431104+DEVARSHI-GOHEL@users.noreply.github.com>
2022-01-12 21:25:38 +0530

    Create day-3.sql
    TCL commands done and scalar function as well as aggregate functions

---
+++ Database Assignment/day-3.sql
@@ -0,0 +1,38 @@
+SELECT COUNT(PRICE) AS 'TOTAL BOOKS' FROM BOOKS
+
+SELECT SUM(PRICE) AS 'TOTAL PRICE' FROM BOOKS
+
+SELECT MAX(PRICE) FROM BOOKS
+
+SELECT MIN(PRICE) FROM BOOKS
+
+SELECT AVG(PRICE) FROM BOOKS
+
+ALTER FUNCTION price(
+    @book_price INT,
+    @discount INT
+)
+RETURNS INT
+AS 
+BEGIN
+    RETURN @book_price * @discount / 100;
+END;
+
+SELECT price(100, 10) as 'Discount';
+
+
+BEGIN TRANSACTION
+	INSERT INTO Authors
+	VALUES
+	(4,'A4')
+
+	DELETE FROM BOOKS
+	WHERE NAME = 'BOOK5'
+
+-- by rollback transaction the database revert back to the state before begin transaction 
+ROLLBACK TRANSACTION
+-- once we commit the transaction we cannot use rollback 
+COMMIT TRANSACTION
+
+SELECT * FROM Authors
+SELECT * FROM BOOKS
