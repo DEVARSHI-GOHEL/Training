08758922a28abce572b8888439d16c59c81307c7
DEVARSHI-GOHEL <97431104+DEVARSHI-GOHEL@users.noreply.github.com>
2022-01-27 19:25:45 +0530

    Updated product task website

--- Web Technologies/JavaScript/Products/ProductForm.js
+++ Web Technologies/JavaScript/Products/ProductForm.js
@@ -1,26 +1,53 @@
-var name;
-var img;
-var desc;
-var quantity;
-var price;
-var dataObj;
+var name = document.getElementById("productName");
+var desc = document.getElementById("productDescription");
+var img = document.getElementById("prdImg");
+var quantity = document.getElementById("productQuantity");
+var price = document.getElementById("productPrice");
+var btn = document.getElementById("submitBtn").value;
+var url, fsize;
 
-function submitList(){
+//To load the image with appropriate path
+function loadFile(event){
+    url = URL.createObjectURL(event.target.files[0]);
+    console.log(url)
+    fsize = (event.target.files[0].size / 1024 / 1024).toFixed(2);
+};
 
-    name = document.getElementById("productName").value;
-    img = document.getElementById("productImage").value;
-    console.log(img);
-    desc = document.getElementById("productDescription").value;
-    quantity = document.getElementById("productQuantity").value;
-    price = document.getElementById("productPrice").value;
+//Submit the data to list
+function submitProduct(){
+    
+    document.getElementById("prdName").innerHTML = document.getElementById("productName").value;
+    document.getElementById("prdDesc").innerText = document.getElementById("productDescription").value;
+    document.getElementById("prdQuantity").innerHTML = document.getElementById("productQuantity").value;
+    document.getElementById("prdPrice").innerHTML = document.getElementById("productPrice").value;
+    img.src = url;
 
-    dataObj = {
-        "name" : name,
-        "img" : img,
-        "desc" : desc,
-        "quantity" : quantity,
-        "price" : price
-    };
+    document.getElementById("productName").value = "";
+    document.getElementById("productDescription").value = "";
+    document.getElementById("productQuantity").value = "";
+    document.getElementById("productPrice").value = "";
+    document.getElementById("productImage").value = "";
+    
+}
 
-    localStorage.setItem("product_data", JSON.stringify(dataObj));
+//Validation of the form
+function validate(){
+    if(document.getElementById("productName").value == ""){
+        alert("You must enter name");
+    }
+    else if(desc.value == ""){
+        alert("You must enter description")
+    }
+    else if(quantity.value <= 0 ){
+        alert("Enter valid choice")
+    }
+    else if(price.value <= 0 ){
+        alert("Enter valid choice")
+    }
+    else if(fsize == null){
+        alert("Upload image")
+    }
+    else{
+        submitProduct()
+    }
 }
